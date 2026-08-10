504020e2e276c473bf5e724a985041e715981f18
peteryu <peteryu@taifex.com.tw>
2026-08-10 12:01:15 +0800

    feat: add Greenplum connection layer

--- src/emip/database/__init__.py
+++ src/emip/database/__init__.py
@@ -1,6 +1,25 @@
-"""Database-layer naming utilities and table constants."""
+"""Database-layer naming, configuration, and connection utilities."""
 
+from .config import (
+    DEFAULT_CONFIG_PATH,
+    DEFAULT_ENV_CONFIG_PATH,
+    DatabaseConfigurationError,
+    DatabaseSettings,
+    load_database_config,
+)
+from .connection import DatabaseConnection, DatabaseConnectionError
 from .naming import DatabaseNaming
 from .tables import OBJECT, OBJECT_VERSION
 
-__all__ = ["DatabaseNaming", "OBJECT", "OBJECT_VERSION"]
+__all__ = [
+    "DEFAULT_CONFIG_PATH",
+    "DEFAULT_ENV_CONFIG_PATH",
+    "DatabaseConfigurationError",
+    "DatabaseConnection",
+    "DatabaseConnectionError",
+    "DatabaseNaming",
+    "DatabaseSettings",
+    "OBJECT",
+    "OBJECT_VERSION",
+    "load_database_config",
+]
